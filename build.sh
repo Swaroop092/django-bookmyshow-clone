b48ce93de55e6e9944e1c565c15f6fea4eddcb05
Swaroop092 <24B11DS007@adityauniversity.in>
2026-07-01 22:09:57 +0530

    auto seed data on deploy

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
-#!/usr/bin/env bash
-#!/usr/bin/env bash
-set -o errexit
-
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
-#python manage.py loaddata data.json
+python manage.py migrate --noinput
+
+python manage.py seed_data
+
+python manage.py collectstatic --noinput
